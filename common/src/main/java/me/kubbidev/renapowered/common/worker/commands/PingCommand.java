a78b946673c1d6f66747383c5d85530e6f23668a
kubbidev <nicoladelaroche@gmail.com>
2024-06-29 00:41:42 +0200

    fix: unused reaction

--- common/src/main/java/me/kubbidev/renapowered/common/worker/commands/PingCommand.java
+++ common/src/main/java/me/kubbidev/renapowered/common/worker/commands/PingCommand.java
@@ -3,7 +3,6 @@ package me.kubbidev.renapowered.common.worker.commands;
 import me.kubbidev.renapowered.common.worker.command.CommandContext;
 import me.kubbidev.renapowered.common.worker.command.DiscordCommand;
 import me.kubbidev.renapowered.common.worker.command.InteractionCommand;
-import net.dv8tion.jda.api.entities.emoji.Emoji;
 import net.dv8tion.jda.api.interactions.commands.build.Commands;
 import net.dv8tion.jda.api.interactions.commands.build.SlashCommandData;
 import net.kyori.adventure.text.Component;
@@ -15,11 +14,7 @@ public class PingCommand implements InteractionCommand {
 
     @Override
     public void onPerform(CommandContext context) {
-        context.reply(context.deferReply(true), Component.text("Pong!"))
-                .thenAccept(message -> {
-                    // add a reaction to let others ping pong
-                    message.addReaction(Emoji.fromUnicode("U+1F3D3")).queue();
-                });
+        context.reply(context.deferReply(true), Component.text("Pong!"));
     }
 
     @Override
