6d7ffab03dc10e9fb61f0a752416d3b2dad49a65
kubbidev <nicoladelaroche@gmail.com>
2024-07-01 17:56:26 +0200

    refactor: change RankingService leaderboard initial day

--- common/src/main/java/me/kubbidev/renapowered/common/module/ranking/RankingService.java
+++ common/src/main/java/me/kubbidev/renapowered/common/module/ranking/RankingService.java
@@ -48,7 +48,7 @@ public class RankingService extends ScheduledTask implements AutoCloseable {
 
     }
 
-    @InitialLocalDay(dayOfWeek = DayOfWeek.TUESDAY)
+    @InitialLocalDay(dayOfWeek = DayOfWeek.MONDAY)
     @InitialLocalTime(hour = 18, minute = 0, second = 0)
     @Override
     public void run() {
